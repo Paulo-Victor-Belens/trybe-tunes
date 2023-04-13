41a95dd3a905838ea390964b0e77551c2f155a94
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-04-13 19:32:53 -0300

    4º requisito concluido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -17,8 +18,9 @@ class Header extends Component {
       isLoading: true,
     });
     const user = await getUser();
+    const { name } = user;
     this.setState({
-      name: user.name,
+      name,
       isLoading: false,
     });
   };
@@ -27,6 +29,11 @@ class Header extends Component {
     const { isLoading, name } = this.state;
     return (
       <header data-testid="header-component">
+        <div>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </div>
         { isLoading
           ? <Loading /> : <div data-testid="header-user-name">{name}</div> }
       </header>
